d90a3c4697b1a6ecfa02449699d5093633e99c03
Denna <lilithprimary@gmail.com>
2022-08-16 13:33:23 +0200

    feat: one more time

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 function App(): JSX.Element {
   return (
-    <>
-      <h1>RSLang</h1>
-      <h1>Hello!!</h1>
-    </>
+    <div>Где конфликты?!?!?!?!</div>
   );
 }
 
